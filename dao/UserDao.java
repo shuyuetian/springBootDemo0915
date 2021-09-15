a6872ce9e1d33d78643a5a5fa3cf2c616a8942c2
shuyuetian <90753228+shuyuetian@users.noreply.github.com>
2021-09-15 17:44:39 +0800

    增加注释
    在线修改

--- dao/UserDao.java
+++ dao/UserDao.java
@@ -4,8 +4,10 @@ import com.openlab.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDao extends JpaRepository<User,Long> {
+    //在线修改
     //通过名字查找
     public User findByName(String name);
     //通过名字和年龄查找
     public User findByNameAndAge(String name,Integer age);
+    
 }
