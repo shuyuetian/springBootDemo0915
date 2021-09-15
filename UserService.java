b3a72aa7f6e266717a269f14e89910c4b0aa5bd2
shuyuetian <1318295580@qq.com>
2021-09-15 16:19:55 +0800

    新增UserService接口

---
+++ UserService.java
@@ -0,0 +1,20 @@
+package com.openlab.service;
+
+import com.openlab.pojo.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface UserService {
+    //查询全部
+    public List<User> findAll();
+    //查询一个
+    public User findUserById(Long id);
+    //新增
+    public void addUser(User user);
+    //删除
+    public void deleteUser(Long id);
+    //修改
+    public void updateUser(Long id,User user);
+
+}
